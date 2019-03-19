54381a24292608e5f2ccccd525baf5ae0c1de9bb
bryanmorse <bryanmorse05@gmail.com>
2019-03-18 18:28:32 -0700

    Refactored logic.  Stopped crashing.  Got aces to calculate properly.  Now attempting to add in the split funcitonality.

--- app/src/main/java/com/blackjack/rainy/bryanmorse/kotlinblackjack/activities/Game_Activity.kt
+++ app/src/main/java/com/blackjack/rainy/bryanmorse/kotlinblackjack/activities/Game_Activity.kt
@@ -2,6 +2,7 @@ package com.blackjack.rainy.bryanmorse.kotlinblackjack.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.blackjack.rainy.bryanmorse.kotlinblackjack.models.Cards
 import com.blackjack.rainy.bryanmorse.kotlinblackjack.R
 import com.blackjack.rainy.bryanmorse.kotlinblackjack.utilities.showBagels
@@ -16,6 +17,8 @@ class Game_Activity : AppCompatActivity() {
     var carPos: Int = 0                     //Current position in the array
     var cardToDealToPlayer: Int = 0
     var cardtoDealtoDealer: Int = 0
+    var playerNumAces: Int = 0
+    var dealerNumAces: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,8 +54,8 @@ class Game_Activity : AppCompatActivity() {
         splitButton.text = ""
 
         //Clear the text
-        dealerHandValue.text = "0"
-        playerHandValue.text = "0"
+        txtDealerHandValue.text = "0"
+        txtPlayerHandValue.text = "0"
 
         //Shuffle the deck
         deckOfCards = Cards.Deck.cards.shuffled()
@@ -60,8 +63,11 @@ class Game_Activity : AppCompatActivity() {
         //Reset the hand values and placements
         playerCurrentHandValue = 0
         dealerCurrentHandValue = 0
-        cardToDealToPlayer = 3      //3 will be the next card handed out to either player
-        cardtoDealtoDealer = 3
+        cardToDealToPlayer = 1
+        cardtoDealtoDealer = 1
+        playerNumAces = 0
+        dealerNumAces = 0
+        carPos = 0
 
         //Begin Game
         hitButton.setOnClickListener {
@@ -76,120 +82,105 @@ class Game_Activity : AppCompatActivity() {
         hitButton.text = getString(R.string.hit_button)
         stayButton.text = getString(R.string.stay_button)
         doubleDownButton.text = getString(R.string.double_down_button)
+        splitButton.text = getString(R.string.split_button)
 
         hitButton.isClickable = true
         stayButton.isClickable = true
         doubleDownButton.isClickable = true
+        splitButton.isClickable = true
+
+
 
         //Display the two player cards
-        playerFirstCard.setImageResource(deckOfCards[carPos].image)
-        playerCurrentHandValue = deckOfCards[carPos].value
-        carPos++
-        playerSecondCard.setImageResource(deckOfCards[carPos].image)
-        playerCurrentHandValue += deckOfCards[carPos].value
-        carPos++
-
-        //Display Player hand value
-        playerHandValue.text = "$playerCurrentHandValue"    //That $ converts it to string
-
-        //Check for Split
-        if (deckOfCards[carPos].name.equals(deckOfCards[carPos--].name)) {
-            splitButton.isClickable = true
-            splitButton.text = getString(R.string.split_button)
-        }
+        nextCard(0, deckOfCards[carPos], playerFirstCard)
+        nextCard(0, deckOfCards[carPos], playerSecondCard)
 
         //Display the first Dealer card
-        dealerFirstCard.setImageResource(deckOfCards[carPos].image)
-        dealerCurrentHandValue = deckOfCards[carPos].value
-        carPos++
-        dealerHandValue.text = "$dealerCurrentHandValue"
+        nextCard(1, deckOfCards[carPos], dealerFirstCard)
 
         //Check to see if Blackjack
         if (playerCurrentHandValue == 21) {
-            endgame(playerCurrentHandValue, dealerCurrentHandValue, true)
+            endGame(playerCurrentHandValue, dealerCurrentHandValue, true)
         }
 
+        /*
+        //Check for Split
+        if (firstCardTest.equals(secondCardTest)) {
+            splitButton.isClickable = true
+            splitButton.text = getString(R.string.split_button)
+        }
+*/
 
 
         //Player hits
         hitButton.setOnClickListener {
 
-            //Disabling the Double option if player chooses to hit
-            if (doubleDownButton.isClickable) {
-                doubleDownButton.isClickable = false
-                doubleDownButton.text = ""
-            }
+            //Disabling the Double and Split options if player chooses to hit
+            doubleDownButton.isClickable = false
+            doubleDownButton.text = ""
+            splitButton.isClickable = false
+            splitButton.text = ""
 
             when (cardToDealToPlayer) {
-                3 -> playerThirdCard.setImageResource(deckOfCards[carPos].image)
-                4 -> playerFourthCard.setImageResource(deckOfCards[carPos].image)
-                5 -> playerFifthCard.setImageResource(deckOfCards[carPos].image)
-                6 -> playerSixthCard.setImageResource(deckOfCards[carPos].image)
+                3 -> nextCard(0, deckOfCards[carPos], playerThirdCard)
+                4 -> nextCard(0, deckOfCards[carPos], playerFourthCard)
+                5 -> nextCard(0, deckOfCards[carPos], playerFifthCard)
+                6 -> nextCard(0, deckOfCards[carPos], playerSixthCard)
                 else -> showBagels("Card limit reached")
             }
-            playerCurrentHandValue += deckOfCards[carPos].value
-            carPos++
-            cardToDealToPlayer++
-            playerHandValue.text = "$playerCurrentHandValue"
-            if (playerCurrentHandValue > 21) {
-//                busted(0)
-                endgame(playerCurrentHandValue, dealerCurrentHandValue, false)
-            }
+
         }
 
-        //Player Doubles Down
+        //Player DOUBLES DOWN
         doubleDownButton.setOnClickListener {
-            playerThirdCard.setImageResource(deckOfCards[carPos].image)
-            playerCurrentHandValue += deckOfCards[carPos].value
-            carPos++
-            playerHandValue.text = "$playerCurrentHandValue"
-            if (playerCurrentHandValue > 21) {
-                endgame(playerCurrentHandValue, dealerCurrentHandValue, false)
-            }
-            else {
-                dealerTurn()
-            }
+            nextCard(0, deckOfCards[carPos], playerThirdCard)       //Player only gets ONE card
+            dealerTurn()
         }
 
-        //Player Stays
+
+
+        //PLAYER STAYS
         stayButton.setOnClickListener {
             dealerTurn()
         }
+
+
+
+        //PLAYER SPLITS
+        splitButton.setOnClickListener {
+
+            nextCard(0, deckOfCards[carPos], playerSecondCard)
+            nextCard(0, deckOfCards[1], playerSplitFirstCard)       //Because we know this is the 2nd card
+            nextCard(0, deckOfCards[carPos], playerSplitSecondCard)
+
+        }
     }
+
+
 
     private fun dealerTurn() {
 
         //"Reveal" the dealer second card
-        dealerSecondCard.setImageResource(deckOfCards[carPos].image)
-        dealerCurrentHandValue += deckOfCards[carPos].value
-        carPos++
-        dealerHandValue.text = "$dealerCurrentHandValue"
+        nextCard(1, deckOfCards[carPos], dealerSecondCard)
 
         while (dealerCurrentHandValue < 17) {
             when (cardtoDealtoDealer) {
-                3 -> dealerThirdCard.setImageResource(deckOfCards[carPos].image)
-                4 -> dealerFourthCard.setImageResource(deckOfCards[carPos].image)
-                5 -> dealerFifthCard.setImageResource(deckOfCards[carPos].image)
-                6 -> dealerSixthCard.setImageResource(deckOfCards[carPos].image)
+                3 -> nextCard(1, deckOfCards[carPos], dealerThirdCard)
+                4 -> nextCard(1, deckOfCards[carPos], dealerFourthCard)
+                5 -> nextCard(1, deckOfCards[carPos], dealerFifthCard)
+                6 -> nextCard(1, deckOfCards[carPos], dealerSixthCard)
                 else -> showBagels("Card limit reached")
             }
-
-            dealerCurrentHandValue += deckOfCards[carPos].value
-            carPos++
-            cardtoDealtoDealer++
         }
 
-        dealerHandValue.text = "$dealerCurrentHandValue"
+        //if dealer doesn't bust, then it's time to determine the winner!
+        endGame(playerCurrentHandValue, dealerCurrentHandValue, false)
 
-        if (dealerCurrentHandValue > 21) {
-            endgame(playerCurrentHandValue, dealerCurrentHandValue, false)
-        } else {
-          //if dealer doesn't bust, then it's time to determine the winner!
-            endgame(playerCurrentHandValue, dealerCurrentHandValue, false)
-        }
     }
 
-    private fun endgame(player: Int, dealer: Int, blackjack: Boolean) {
+
+
+    private fun endGame(player: Int, dealer: Int, blackjack: Boolean) {
         when {
             blackjack -> showBagels("BLACKJACK!")
             player > 21 -> showBagels("Player Bust")
@@ -202,6 +193,8 @@ class Game_Activity : AppCompatActivity() {
 
         deactivateButtons()
     }
+
+
 
     private fun deactivateButtons() {
 
@@ -219,6 +212,70 @@ class Game_Activity : AppCompatActivity() {
         //Reset the game back to start
         hitButton.setOnClickListener {
             resetGame()
+        }
+    }
+
+
+
+    private fun nextCard(who: Int, card: Cards, location: ImageView) {
+
+        location.setImageResource(card.image)       //Display card
+
+        if (who == 0) {
+            //PLAYER
+            playerCurrentHandValue += card.value                    //Update hand value
+            txtPlayerHandValue.text = "$playerCurrentHandValue"     //Display value.  That $ converts it to string
+            if (card.name.equals("Ace")) {
+                playerNumAces++                                     //Update the number of aces
+            }
+            cardToDealToPlayer++                                    //Next position for the image to display
+
+            if (playerCurrentHandValue > 21) {                      //Check total value
+                checkForAces(0, playerNumAces)                //Check to see if player has an aces
+            }
+        }
+
+        else {
+            //DEALER
+            dealerCurrentHandValue += card.value
+            txtDealerHandValue.text = "$dealerCurrentHandValue"
+            if (card.name.equals("Ace")) {
+                dealerNumAces++                                     //Update number of aces
+            }
+            cardtoDealtoDealer++                                    //Next position for the image to display
+
+            if (dealerCurrentHandValue > 21) {                      //Check total value
+                checkForAces(1, dealerNumAces)                //Check to see if player has an aces
+            }
+        }
+
+        carPos++                                                    //Next position in the array for the deck
+    }
+
+
+
+    private fun checkForAces(who: Int, number: Int) {
+
+        if (who == 0) {
+            if (number > 0) {
+                playerCurrentHandValue -= 10
+                playerNumAces--
+                txtPlayerHandValue.text = "$playerCurrentHandValue"
+            }
+            else {
+                endGame(playerCurrentHandValue, dealerCurrentHandValue, false)  //If no aces, end the game
+            }
+        }
+
+        else {
+            if (number > 0) {
+                dealerCurrentHandValue -= 10
+                dealerNumAces--
+                txtDealerHandValue.text = "$dealerCurrentHandValue"
+            }
+            else {
+                endGame(playerCurrentHandValue, dealerCurrentHandValue, false)  //If no aces, end the game
+            }
         }
     }
 
--- app/src/main/java/com/blackjack/rainy/bryanmorse/kotlinblackjack/models/Cards.kt
+++ app/src/main/java/com/blackjack/rainy/bryanmorse/kotlinblackjack/models/Cards.kt
@@ -2,7 +2,7 @@ package com.blackjack.rainy.bryanmorse.kotlinblackjack.models
 
 import com.blackjack.rainy.bryanmorse.kotlinblackjack.R
 
-data class Cards(var suite: String, var name: String, var value: Int, var image: Int, var isAce: Boolean) {
+data class Cards(var suite: String, var name: String, var value: Int, var image: Int) {
     
     object Deck {
         
@@ -11,313 +11,313 @@ data class Cards(var suite: String, var name: String, var value: Int, var image:
                 "Hearts",
                 "Ace",
                 11,
-                R.drawable.heartsace, true
+                R.drawable.heartsace
             ),
             Cards(
                 "Hearts",
                 "Two",
                 2,
-                R.drawable.hearts2, false
+                R.drawable.hearts2
             ),
             Cards(
                 "Hearts",
                 "Three",
                 3,
-                R.drawable.hearts3, false
+                R.drawable.hearts3
             ),
             Cards(
                 "Hearts",
                 "Four",
                 4,
-                R.drawable.hearts4, false
+                R.drawable.hearts4
             ),
             Cards(
                 "Hearts",
                 "Five",
                 5,
-                R.drawable.hearts5, false
+                R.drawable.hearts5
             ),
             Cards(
                 "Hearts",
                 "Six",
                 6,
-                R.drawable.hearts6, false
+                R.drawable.hearts6
             ),
             Cards(
                 "Hearts",
                 "Seven",
                 7,
-                R.drawable.hearts7, false
+                R.drawable.hearts7
             ),
             Cards(
                 "Hearts",
                 "Eight",
                 8,
-                R.drawable.hearts8, false
+                R.drawable.hearts8
             ),
             Cards(
                 "Hearts",
                 "Nine",
                 9,
-                R.drawable.hearts9, false
+                R.drawable.hearts9
             ),
             Cards(
                 "Hearts",
                 "Ten",
                 10,
-                R.drawable.hearts10, false
+                R.drawable.hearts10
             ),
             Cards(
                 "Hearts",
                 "Jack",
                 10,
-                R.drawable.jack_of_hearts, false
+                R.drawable.jack_of_hearts
             ),
             Cards(
                 "Hearts",
                 "Queen",
                 10,
-                R.drawable.queen_of_hearts, false
+                R.drawable.queen_of_hearts
             ),
             Cards(
                 "Hearts",
                 "King",
                 10,
-                R.drawable.king_of_hearts, false
+                R.drawable.king_of_hearts
             ),
             Cards(
                 "Clubs",
                 "Ace",
                 11,
-                R.drawable.clubsace, true
+                R.drawable.clubsace
             ),
             Cards(
                 "Clubs",
                 "Two",
                 2,
-                R.drawable.clubs2, false
+                R.drawable.clubs2
             ),
             Cards(
                 "Clubs",
                 "Three",
                 3,
-                R.drawable.clubs3, false
+                R.drawable.clubs3
             ),
             Cards(
                 "Clubs",
                 "Four",
                 4,
-                R.drawable.clubs4, false
+                R.drawable.clubs4
             ),
             Cards(
                 "Clubs",
                 "Five",
                 5,
-                R.drawable.clubs5, false
+                R.drawable.clubs5
             ),
             Cards(
                 "Clubs",
                 "Six",
                 6,
-                R.drawable.clubs6, false
+                R.drawable.clubs6
             ),
             Cards(
                 "Clubs",
                 "Seven",
                 7,
-                R.drawable.clubs7, false
+                R.drawable.clubs7
             ),
             Cards(
                 "Clubs",
                 "Eight",
                 8,
-                R.drawable.clubs8, false
+                R.drawable.clubs8
             ),
             Cards(
                 "Clubs",
                 "Nine",
                 9,
-                R.drawable.clubs9, false
+                R.drawable.clubs9
             ),
             Cards(
                 "Clubs",
                 "Ten",
                 10,
-                R.drawable.clubs10, false
+                R.drawable.clubs10
             ),
             Cards(
                 "Clubs",
                 "Jack",
                 10,
-                R.drawable.jack_of_clubs, false
+                R.drawable.jack_of_clubs
             ),
             Cards(
                 "Clubs",
                 "Queen",
                 10,
-                R.drawable.queen_of_clubs, false
+                R.drawable.queen_of_clubs
             ),
             Cards(
                 "Clubs",
                 "King",
                 10,
-                R.drawable.king_of_clubs, false
+                R.drawable.king_of_clubs
             ),
             Cards(
                 "Diamonds",
                 "Ace",
                 11,
-                R.drawable.diamondsace, true
+                R.drawable.diamondsace
             ),
             Cards(
                 "Diamonds",
                 "Two",
                 2,
-                R.drawable.diamonds2, false
+                R.drawable.diamonds2
             ),
             Cards(
                 "Diamonds",
                 "Three",
                 3,
-                R.drawable.diamonds3, false
+                R.drawable.diamonds3
             ),
             Cards(
                 "Diamonds",
                 "Four",
                 4,
-                R.drawable.diamonds4, false
+                R.drawable.diamonds4
             ),
             Cards(
                 "Diamonds",
                 "Five",
                 5,
-                R.drawable.diamonds5, false
+                R.drawable.diamonds5
             ),
             Cards(
                 "Diamonds",
                 "Six",
                 6,
-                R.drawable.diamonds6, false
+                R.drawable.diamonds6
             ),
             Cards(
                 "Diamonds",
                 "Seven",
                 7,
-                R.drawable.diamonds7, false
+                R.drawable.diamonds7
             ),
             Cards(
                 "Diamonds",
                 "Eight",
                 8,
-                R.drawable.diamonds8, false
+                R.drawable.diamonds8
             ),
             Cards(
                 "Diamonds",
                 "Nine",
                 9,
-                R.drawable.diamonds9, false
+                R.drawable.diamonds9
             ),
             Cards(
                 "Diamonds",
                 "Ten",
                 10,
-                R.drawable.diamonds10, false
+                R.drawable.diamonds10
             ),
             Cards(
                 "Diamonds",
                 "Jack",
                 10,
-                R.drawable.jack_of_diamonds, false
+                R.drawable.jack_of_diamonds
             ),
             Cards(
                 "Diamonds",
                 "Queen",
                 10,
-                R.drawable.queen_of_diamonds, false
+                R.drawable.queen_of_diamonds
             ),
             Cards(
                 "Diamonds",
                 "King",
                 10,
-                R.drawable.king_of_diamonds, false
+                R.drawable.king_of_diamonds
             ),
             Cards(
                 "Spades",
                 "Ace",
                 11,
-                R.drawable.spadesace, true
+                R.drawable.spadesace
             ),
             Cards(
                 "Spades",
                 "Two",
                 2,
-                R.drawable.spades2, false
+                R.drawable.spades2
             ),
             Cards(
                 "Spades",
                 "Three",
                 3,
-                R.drawable.spades3, false
+                R.drawable.spades3
             ),
             Cards(
                 "Spades",
                 "Four",
                 4,
-                R.drawable.spades4, false
+                R.drawable.spades4
             ),
             Cards(
                 "Spades",
                 "Five",
                 5,
-                R.drawable.spades5, false
+                R.drawable.spades5
             ),
             Cards(
                 "Spades",
                 "Six",
                 6,
-                R.drawable.spades6, false
+                R.drawable.spades6
             ),
             Cards(
                 "Spades",
                 "Seven",
                 7,
-                R.drawable.spades7, false
+                R.drawable.spades7
             ),
             Cards(
                 "Spades",
                 "Eight",
                 8,
-                R.drawable.spades8, false
+                R.drawable.spades8
             ),
             Cards(
                 "Spades",
                 "Nine",
                 9,
-                R.drawable.spades9, false
+                R.drawable.spades9
             ),
             Cards(
                 "Spades",
                 "Ten",
                 10,
-                R.drawable.spades10, false
+                R.drawable.spades10
             ),
             Cards(
                 "Spades",
                 "Jack",
                 10,
-                R.drawable.jack_of_spades, false
+                R.drawable.jack_of_spades
             ),
             Cards(
                 "Spades",
                 "Queen",
                 10,
-                R.drawable.queen_of_spades, false
+                R.drawable.queen_of_spades
             ),
             Cards(
                 "Spades",
                 "King",
                 10,
-                R.drawable.king_of_spades, false
+                R.drawable.king_of_spades
             )
         
         )
